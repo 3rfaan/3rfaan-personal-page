a64c0e6d13250acaa20aa9cc8c330cb42b67cd46
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-05-12 18:02:46 +0200

    Bug fix in 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,9 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { useContext } from "react";
-import { BiArrowBack } from "react-icons/bi";
-import { IoMdArrowForward } from "react-icons/io";
 import Intro from "../components/Intro";
 import styles from "../styles/TagPage.module.css";
-import { LanguageContext } from "../utils/context/languageContext";
-import { ar, en } from "../utils/translations";
+import GoBack from "../components/GoBack";
 
 const NotFound = () => {
-  const { language } = useContext(LanguageContext);
-  const router = useRouter();
   return (
     <main className={styles.tagPage}>
       <Head>
@@ -22,20 +15,7 @@ const NotFound = () => {
 
       <div className={styles.container}>
         <h1 className={styles.queryTitle}>404 - Not found!</h1>
-        {language === "english" ? (
-          <div className={styles.goBackContainer} onClick={() => router.back()}>
-            <BiArrowBack />
-            <h4>{en.tagPage.goBack}</h4>
-          </div>
-        ) : (
-          <div
-            className={styles.ar_goBackContainer}
-            onClick={() => router.back()}
-          >
-            <IoMdArrowForward />
-            <h4>{ar.tagPage.goBack}</h4>
-          </div>
-        )}
+        <GoBack />
       </div>
     </main>
   );
